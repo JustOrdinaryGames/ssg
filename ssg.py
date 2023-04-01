dcf8efdb0f8a5f4014ea24ae2fd58a38d63d95b9
Sebastian <129621930+sbstnlol@users.noreply.github.com>
2023-04-01 17:51:57 -0300

    Removed duplicate code

--- ssg.py
+++ ssg.py
@@ -2,6 +2,11 @@ import os
 import shutil
 import markdown
 
+version = "1.0"
+
+print("SSG v" + version)
+print("---")
+
 if os.path.exists('output'):
     shutil.rmtree('output')
 
@@ -21,32 +26,24 @@ for root, dirs, files in os.walk('content'):
         full_file_path = os.path.join(root, filename)
         output_path = full_file_path.replace('.md', '.html').replace('.markdown', '.html').replace('content/', 'output/')
 
+        if not os.path.exists(output_path):
+            os.makedirs(output_path.replace('/' + file.replace('.md', '.html').replace('.markdown', '.html'), ''), exist_ok=True)
+        
+        reading_file = open(full_file_path, "r")
+        file_content = reading_file.read()
+        reading_file.close()
+        formatted_content = markdown.markdown(file_content)
+
+        output_file = open(output_path, "w+")
+
         if filename.endswith(".md") or filename.endswith(".markdown"):
-            reading_file = open(full_file_path, "r")
-            file_content = reading_file.read()
-            reading_file.close()
-            formatted_content = markdown.markdown(file_content)
-
-            print(full_file_path + " as " + output_path)
-
-            if not os.path.exists(output_path):
-                os.makedirs(output_path.replace('/' + file.replace('.md', '.html').replace('.markdown', '.html'), ''), exist_ok=True)
-
-            output_file = open(output_path, "w+")
-
             output_file.write(top_content + formatted_content + bottom_content)
-            output_file.close()
         else:
-            reading_file = open(full_file_path, "r")
-            file_content = reading_file.read()
-            reading_file.close()
-
-            if not os.path.exists(output_path):
-                os.makedirs(output_path.replace('/' + file.replace('.md', '.html').replace('.markdown', '.html'), ''), exist_ok=True)
-
-            output_file = open(output_path, "w+")
-
             output_file.write(top_content + file_content + bottom_content)
-            output_file.close()
+        
+        output_file.close()
 
+        print(full_file_path + " as " + output_path)
+
+print("---")
 print("Everything should be finished now. Enjoy your website!")
